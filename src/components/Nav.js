a5cce166b9cd29d8234483ac2a356b4f6c915d68
Nick Colavita <nickcolavita@Nicks-MBP.fios-router.home>
2020-04-16 10:14:34 -0400

    cleaning comments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,28 +3,6 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        //     <div className="d-flex justify-content-start">
-        //         <Link to="/" style={linkStyle}>
-        //             <strong>Pm</strong>
-        //         </Link>
-        //     </div>
-        //     <div className="d-flex justify-content-end">
-        //         <ul className="navbar-nav">
-        //             <li className="nav-item">
-        //                 <Link to="/" style={linkStyle}>
-        //                     By Ingredient
-        //                 </Link>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <Link to="/nutritionalsearch" style={linkStyle}>
-        //                     By Nutrition
-        //                 </Link>
-        //             </li>
-        //         </ul>
-        //     </div>
-        // </nav>
-
         <div style={navStyle.container}>
             <div style={navStyle.logoContainer}>
                 <Link to="/" style={navStyle.link}>
