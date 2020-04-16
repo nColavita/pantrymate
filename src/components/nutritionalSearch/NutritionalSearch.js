755cdf4553a36f2d91d9bbf25e61e46381f0498f
Nick Colavita <nickcolavita@Nicks-MBP.fios-router.home>
2020-04-16 09:30:13 -0400

    creating a Utils functional component to share nutritional array for component cleanup

--- src/components/nutritionalSearch/NutritionalSearch.js
+++ src/components/nutritionalSearch/NutritionalSearch.js
@@ -2,80 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import NutritionalSearchField from './NutritionalSearchField';
+import { returnSearchFields } from '../../utils/Utils';
 
 class NutritionalSearch extends Component {
-    fields = [
-        {
-            id: 1,
-            name: 'Cuisine',
-            searchItems: [
-                '',
-                'African',
-                'American',
-                'British',
-                'Cajun',
-                'Caribbean',
-                'Chinese',
-                'Eastern European',
-                'European',
-                'French',
-                'German',
-                'Greek',
-                'Indian',
-                'Irish',
-                'Italian',
-                'Japanese',
-                'Jewish',
-                'Korean',
-                'Latin American',
-                'Mediterranean',
-                'Mexican',
-                'Middle Eastern',
-                'Nordic',
-                'Southern',
-                'Spanish',
-                'Thai',
-                'Vietnamese',
-            ],
-        },
-        {
-            id: 2,
-            name: 'Diet',
-            searchItems: [
-                '',
-                'Gluent Free',
-                'Ketogenic',
-                'Vegetarian',
-                'Lacto-Vegetarian',
-                'Ovo-Vegetarian',
-                'Vegan',
-                'Pescetarian',
-                'Paleo',
-                'Primal',
-                'Whole30',
-            ],
-        },
-        {
-            id: 3,
-            name: 'Intolerance',
-            searchItems: [
-                '',
-                'Dairy',
-                'Egg',
-                'Gluten',
-                'Grain',
-                'Peanut',
-                'Seafood',
-                'Sesame',
-                'Shellfish',
-                'Soy',
-                'Sulfite',
-                'Tree Nut',
-                'Wheat',
-            ],
-        },
-    ];
-
     componentDidMount() {
         this.props.handlePageChange();
     }
@@ -90,7 +19,7 @@ class NutritionalSearch extends Component {
                 className="container"
                 style={{ width: '65%', maxWidth: '600px' }}
             >
-                {this.fields.map((field) => (
+                {returnSearchFields().map((field) => (
                     <NutritionalSearchField
                         key={field.id}
                         name={field.name}
